3b149400bef57c9ff2514cd23f09917180b81617
ArKrasavin <arkrasavin.ak@gmail.com>
2024-09-27 20:32:03 +0300

    Добавление create superuser

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+import os
+
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+django.setup()
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'email')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if password is None:
+    raise ValueError('Необходимо задать переменную окружения DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username='admin').exsists():
+    User.objects.create_superuser(username=username, email=email, password=password)
