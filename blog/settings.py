81d313eee278b05b3d6c875bda649809baea8ef8
ArKrasavin <arkrasavin.ak@gmail.com>
2024-09-27 17:58:00 +0300

    Правка настроек 2

--- blog/settings.py
+++ blog/settings.py
@@ -83,18 +83,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default':
-        dj_database_url.config(
-            default=os.environ.get('DATABASE_URL')
-        ),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
